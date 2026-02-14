1f7157b1bc375c2ff611dd15ff411b5ec93eec86
TomMcFox <fux.mobil@gmail.com>
2026-02-14 18:21:04 +0100

    2026_v4

--- login_form.php
+++ login_form.php
@@ -1,15 +1,16 @@
-
+<?php
+$config = require __DIR__ . '/config/index.php';
+$login =  require __DIR__ . '/config/login.php';
+?>
 <form class="login-form" action="" method="POST">
-    <h2></h2>
+    <h2><?= htmlspecialchars($config['titel']) ?></h2>
     <div class="input-group">
         <label for="username">Benutzername</label>
-        <input type="text" id="username" name="username" required value="Gast">
+        <input type="text" id="username" name="username" required value="<?= htmlspecialchars($login['user']) ?>">
     </div>
     <div class="input-group">
         <label for="password">Passwort</label>
-        <input type="password" id="password" name="password" required value="Passwort">
+        <input type="password" id="password" name="password" required value="<?= htmlspecialchars($login['passwort']) ?>">
     </div>
     <button type="submit">Anmelden</button>
 </form>
-
-
